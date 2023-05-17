2bba33c2bb624772f229c1f612a6e4c25f79200b
Arosaleskdoce <arosales@kdoce.cl>
2023-05-17 17:45:10 -0400

    A.R Validaciones y funciones del inventario

--- js/scripts.js
+++ js/scripts.js
@@ -32,15 +32,22 @@ formulario.addEventListener("submit", function (evento) {
 
   const { nombre, apellidos, telefono, correo, mensaje } = datos;
 
-  if (nombre === '') {
-    console.log(' El nombre esta vacio flojo');
+  if (
+    nombre === "" ||
+    apellidos === "" ||
+    telefono === "" ||
+    correo === "" ||
+    mensaje === ""
+  ) {
+    mostrarAlerta("Todos los campos son obligatorios", true);
 
     return;
   }
 
-  //Enviar el formulario
+  //Alerta de enviar formulario correctamente
+  mostrarAlerta("Mensaje enviado correctamente");
 
-  console.log("Enviado Formulario");
+  // console.log("Enviando Formulario");
 });
 
 // Funciones
@@ -48,5 +55,56 @@ formulario.addEventListener("submit", function (evento) {
 function leerTexto(e) {
   datos[e.target.id] = e.target.value;
 
-  console.log(datos);
+  // console.log(datos);
 }
+
+//blablablabla
+function mostrarAlerta(mensaje, error = null) {
+  const alerta = document.createElement('P');
+  alerta.textContent = mensaje;
+
+  if (error) {
+    alerta.classList.add('error');
+  } else {
+    alerta.classList.add('correcto');
+  }
+
+  formulario.appendChild(alerta);
+
+    //Efecto desaparecer despues de 5seg
+  setTimeout(() => {
+    alerta.remove();
+  }, 5000);
+
+}
+
+//Funcion de alerta cuando se envia correctamente
+// function mostrarMensaje(mensaje) {
+
+//   //Efecto des
+//   setTimeout(() => {
+//     alerta.remove();
+//   }, 5000);
+// }
+
+// //Funcion de error en pantalla
+// function mostrarError(mensaje) {
+
+//   //Efecto desaparecer despues de 5seg
+//   setTimeout(() => {
+//     error.remove();
+//   }, 5000);
+// }
+
+ //Validaciones de reglas de Negocio
+
+// function validarFormatoCorreoElectronico(correo) {
+//   var formato = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   return formato.test(correo);
+// }
+
+// //Uso de la función de validación
+// var email = "correo@example.com";
+// if (!validarFormatoCorreoElectronico(correo)) {
+//   console.log("El correo electrónico no tiene un formato válido.");
+// }
